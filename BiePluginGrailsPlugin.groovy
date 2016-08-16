9940ede750170af3a8662b48837290ef4396bb90
charvolant <doug@charvolant.org>
2016-08-16 16:37:00 +1000

    Release version

--- BiePluginGrailsPlugin.groovy
+++ BiePluginGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import grails.util.Environment
 class BiePluginGrailsPlugin {
 
     // the plugin version
-    def version = "1.1-SNAPSHOT"
+    def version = "1.2.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
