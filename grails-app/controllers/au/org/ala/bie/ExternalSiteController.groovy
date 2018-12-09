5a208a6e3f75e821ed1b518f8d42e674e563ec63
Doug Palmer <Doug.Palmer@csiro.au>
2018-12-10 09:58:19 +1100

    Handle empty return from EoL Use bie-index to restruct/sort child concepts rather than get clever in gsp

--- grails-app/controllers/au/org/ala/bie/ExternalSiteController.groovy
+++ grails-app/controllers/au/org/ala/bie/ExternalSiteController.groovy
@@ -34,15 +34,14 @@ class ExternalSiteController {
         log.debug "Initial EOL url = ${searchURL}"
         def js = new JsonSlurper()
         def jsonText = new java.net.URL(searchURL).text
-
-        def json = js.parseText(jsonText)
+        def json = js.parseText(jsonText ?: '{}')
 
         //get first pageId
         if(json.results){
             def pageId = json.results[0].id
             def pageUrl = "http://eol.org/api/pages/1.0/${pageId}.json?images=00&videos=0&sounds=0&maps=0&text=2&iucn=false&subjects=overview&licenses=all&details=true&references=true&vetted=0&cache_ttl="
             log.debug(pageUrl)
-            def pageText = new java.net.URL(pageUrl).text
+            def pageText = new java.net.URL(pageUrl).text ?: '{}'
             response.setContentType("application/json")
             render pageText
         } else {
