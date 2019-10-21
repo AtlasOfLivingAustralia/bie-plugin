2137e51339d300bcbc1cc654c7cf4ee1283682fe
vjrj <vjrj@ourproject.org>
2019-10-21 21:25:52 +0200

    Usage of external ala-i18n resources

--- grails-app/init/bie/plugin/BootStrap.groovy
+++ grails-app/init/bie/plugin/BootStrap.groovy
@@ -1,8 +1,18 @@
 package bie.plugin
 
 class BootStrap {
+    def messageSource
 
     def init = { servletContext ->
+        messageSource.setBasenames(
+                // For some reason, setting basenames in BiePluginGrailsPlugin
+                // does not work as expected extending PluginAwareResourceBundleMessageSource
+                // https://stackoverflow.com/questions/16063391/grails-override-a-bean-property-value-in-resources-groovy
+                // so we do here that works:q
+                "file:///var/opt/atlas/i18n/bie-plugin/messages",
+                "file:///opt/atlas/i18n/bie-plugin/messages",
+                "classpath:grails-app/i18n/messages",
+        )
         Object.metaClass.trimLength = { Integer stringLength ->
 
             String trimString = delegate?.toString()
